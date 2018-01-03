64bb863fd95f7fd56ecc6424cdf029e7f7ade1f5
杨珂 <yangke@yangkedeMac-mini.local>
2018-01-03 11:02:32 +0800

    第一次更改

--- HRZ/ViewController.m
+++ HRZ/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"第二");
+    NSArray *Arr = [NSArray new];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
